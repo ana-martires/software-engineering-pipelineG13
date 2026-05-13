80f5bf67bd62408197640653c9fe9ad3ddcd4727
GuillemGonzalezCasanova <guillem.gonzalez04@estudiant.upf.edu>
2026-05-13 18:08:25 +0200

    Fix of general style

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
-from calculator import add, sub, factorial
-from calculator import add, sub, squared
+from calculator import add, sub, factorial, division, multiply, squared
+
 
 def test_add():
     assert add(2, 3) == 5
@@ -8,15 +8,20 @@ def test_add():
 def test_sub():
     assert sub(10, 4) == 6
 
+
 def test_factorial():
     assert factorial(0) == 1
     assert factorial(1) == 1
     assert factorial(5) == 120
+
+
 def test_division():
     assert division(4, 2) == 2
  
-def test_multiply ():
+
+def test_multiply():
     assert multiply(2, 5) == 10
+
 
 def test_squared():
     assert squared(4) == 16
