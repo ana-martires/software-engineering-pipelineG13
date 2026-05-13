363b3ddcf1d13960c3a0851c7b83bcbf9f41453f
BradDanAlexandru <brad.dan01@estudiant.upf.edu>
2026-05-13 16:00:26 +0200

    factorial

--- calculator.py
+++ calculator.py
@@ -4,3 +4,12 @@ def add(a, b):
 
 def sub(a, b):
     return a - b
+
+def factorial(a):
+    if a == 0 or a == 1:
+        return 1
+    else: 
+        result = 1
+        for i in range(2, a + 1):
+            result *= i
+        return result
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, sub
+from calculator import add, sub, factorial
 
 def test_add():
     assert add(2, 3) == 5
@@ -6,3 +6,8 @@ def test_add():
 
 def test_sub():
     assert sub(10, 4) == 6
+
+def test_factorial():
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(5) == 120
